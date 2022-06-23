f5393676984c130314200ef365c9fcb234e670a3
fft <xf.fifteen@gmail.com>
2022-06-23 16:06:13 +0800

    add env.sh

---
+++ env.sh
@@ -0,0 +1,4 @@
+export CMAKE_PREFIX_PATH=/opt/ros/noetic
+export LD_LIBRARY_PATH=/home/fft/Qt5.14.2/5.14.2/gcc_64/lib/:/opt/ros/noetic/lib/:/usr/lib/x86_64-linux-gnu/:/opt/ros/noetic/lib:/usr/share/ewaybot/thirdpartylib/lib/ros/:/home/fft/Qt5.14.2/5.14.2/gcc_64/lib
+export PATH=/opt/ros/noetic/bin:/home/fft/Qt5.14.2/5.14.2/gcc_64/bin:/usr/bin:/home/fft/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export ROS_PACKAGE_PATH=/opt/ros/noetic/share
